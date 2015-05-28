182ccbe003d04a962f9420b3e9a819057f96555d
pihizi <pihizi@msn.com>
2015-05-28 17:29:42 +0800

    gini doctor

--- class/Gini/Module/MallHubAPI.php
+++ class/Gini/Module/MallHubAPI.php
@@ -11,4 +11,29 @@ class MallHubAPI
         class_exists('\Gini\Those');
         class_exists('\Gini\ThoseIndexed');
     }
+
+    public static function diagnose()
+    {
+        // check database: SHOW TABLES?
+        $db = \Gini\Database::db();
+        $ret = $db->query('SHOW TABLES');
+        if (!$ret) {
+            return ['Please config your database in raw/config/@'
+                .($_SERVER['GINI_ENV'] ?: 'production').'/database.yml!', ];
+        }
+
+        // check gapper setting: is a valid app registered in gapper
+        try {
+            $conf = \Gini\Config::get('gapper.rpc');
+            $rpc = \Gini\IoC::construct('\Gini\RPC', $conf['url']);
+            $bool = $rpc->gapper->app->authorize((string)$conf['client_id'], (string)$conf['client_secret']);
+            $bool = $bool ?: ($bool===false?:false);
+            if ($bool) {
+                return ['Please check your rpc config in gapper.yml!'];
+            }
+        }
+        catch (\Exception $e) {
+                return ['gapper.rpc: '.$e->getMessage()];
+        }
+    }
 }
